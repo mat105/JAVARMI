3a244d8bd0bf87fc2c44e9f65364d891beb64c32
mat105 <mat.105@hotmail.com>
2016-03-22 15:39:10 -0300

    Agregado clima

---
+++ JAVARMI/Clima.java
@@ -0,0 +1,12 @@
+
+public class Clima {
+	private float temperatura;
+	private String tiempo;
+	
+	public Clima(float temperatura, String tiempo) {
+		super();
+		this.temperatura = temperatura;
+		this.tiempo = tiempo;
+	}
+	
+}
